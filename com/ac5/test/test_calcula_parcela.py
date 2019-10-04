550e0e17dcdf3c51b3c739faf8b79a55ab7093c7
Erick S <35637498+EquinhoBr@users.noreply.github.com>
2019-10-03 22:32:59 -0300

    Update test_calcula_parcela.py

--- com/ac5/test/test_calcula_parcela.py
+++ com/ac5/test/test_calcula_parcela.py
@@ -1,12 +1,15 @@
 from com.ac5.calcula_parcela import valor_pagamento
-
+"""Calcula"""
 def test_valorcerto():
     assert valor_pagamento(8, 5) == 8.64, "Deveria ser 8.64"
+    """Calcula"""
 
 
 def test_valormenor():
-    assert valor_pagamento(-2, 5) == None, "Deveria ser None"
+    assert valor_pagamento(-2, 5) == , "Deveria ser None"
+    """Calcula"""
 
 
 def test_diazero():
     assert valor_pagamento(10, 0) == 10, "Deveria ser 5"
+    """Calcula"""
