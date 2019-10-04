58f607a9d7cc1d6f614bdbbc102be792ac0843e0
thittp <53881782+thittp@users.noreply.github.com>
2019-10-03 21:57:58 -0300

    Update test_conta_corrente.py

--- com/ac5/test/test_conta_corrente.py
+++ com/ac5/test/test_conta_corrente.py
@@ -1,11 +1,13 @@
 from com.ac5.conta_corrente import conta_corrente
 
 def test_dep():
+    '''Esta função faz deposito'''
     conta = conta_corrente(10, 'Erick')
     conta.deposito(10)
     assert conta.saldo == 10
 
 def test_saque():
+    '''Esta função faz saque'''
     conta = conta_corrente(20, 'Thiago', 5)
     conta.saque(5)
     assert conta.saldo == 0
