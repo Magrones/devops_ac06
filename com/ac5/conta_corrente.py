9f816d70e86721147161c4bc0c80fcc23fd41119
thittp <53881782+thittp@users.noreply.github.com>
2019-10-03 22:28:05 -0300

    Update conta_corrente.py

--- com/ac5/conta_corrente.py
+++ com/ac5/conta_corrente.py
@@ -2,15 +2,19 @@
 class conta_corrente:
 
     def __init__(self, numero, nome_correntista, saldo=0.0):
+        '''Conta corrente'''
         self.numero = numero
         self.alterar_nome(nome_correntista)
         self.saldo = saldo
 
     def alterar_nome(self, nome_correntista):
+        '''correntista'''
         self.nome_correntista = nome_correntista
 
     def deposito(self, valor):
+        '''deposito'''
         self.saldo += valor
 
     def saque(self, valor):
+        '''saque'''
         self.saldo -= valor
