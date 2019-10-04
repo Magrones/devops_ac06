e05b51dafe9dca113cecb38a2db2ab1446591554
thittp <53881782+thittp@users.noreply.github.com>
2019-10-03 21:52:46 -0300

    Update test_conta_corrente.py

--- com/ac5/test/test_conta_corrente.py
+++ com/ac5/test/test_conta_corrente.py
@@ -1,11 +1,11 @@
-from com.ac5.conta_corrente import Conta_Corrente
+from com.ac5.conta_corrente import conta_corrente
 
 def test_dep():
-    conta = Conta_Corrente(10, 'Erick')
+    conta = conta_corrente(10, 'Erick')
     conta.deposito(10)
     assert conta.saldo == 10
 
 def test_saque():
-    conta = Conta_Corrente(20, 'Thiago', 5)
+    conta = conta_corrente(20, 'Thiago', 5)
     conta.saque(5)
     assert conta.saldo == 0
