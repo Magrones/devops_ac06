31789a66c840db9a96249fbbaeaf54ce41a9ccae
FelipeAdduci <53922482+FelipeAdduci@users.noreply.github.com>
2019-10-03 22:24:28 -0300

    Update calcula_parcela.py

--- com/ac5/calcula_parcela.py
+++ com/ac5/calcula_parcela.py
@@ -1,8 +1,7 @@
 'Isso faz algo legal'
 def valor_pagamento(valor, dias_atraso):
-    
-     if valor < 0:
-        return None
+    if valor < 0:
+         return None
     if dias_atraso > 0:
         multa = valor * 0.03
         adicional_atraso = valor * dias_atraso * 0.01
